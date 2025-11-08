b4dec423259eccb77f490a759d31986e671fbb79
rina <kulikova.arina.d@gmail.com>
2025-11-09 00:29:38 +0300

    vse rabotaet

--- орг.py
+++ орг.py
@@ -9,7 +9,7 @@ def pars(url):
     return table
 
 
-url='https://docs.google.com/spreadsheets/d/1hHUUdHhO7uMSr-kFrAed1NVeA2vD-2NNVqyJQCJ2X9w/export?format=csv&id=1hHUUdHhO7uMSr-kFrAed1NVeA2vD-2NNVqyJQCJ2X9w&gid=474547065'
+url ='https://docs.google.com/spreadsheets/d/1unVwdDM0pBJEUeO6tCCZowzM8at2Db4HCVz_8zpEJms/export?format=csv&id=1unVwdDM0pBJEUeO6tCCZowzM8at2Db4HCVz_8zpEJms&gid=0'
 table=pars(url)
 print (table)
 
@@ -20,8 +20,14 @@ def irr(table):
     for line in table.split('\n'):
         if lastname in line:
             try:
-                line=line.split(',')
-                return line[-1]
+                if '"' in line:
+                    line=line.split('"')
+                    res=line[-2]
+                else:
+                    line=line.split(',')
+                    res=line[-2]
+                return res
+
             except (IndexError, ValueError):
                 return 'Ошибка обработки данных'
     return 'ты не в этой группе'
